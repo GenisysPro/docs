156a0fe3a4c71e9ed78efa0f9307c08664b80658
Leo <Leo3418@users.noreply.github.com>
2017-08-03 16:15:12 +0800

    Add comments for build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+# Get GenisysPro source code for building
 git clone https://github.com/GenisysPro/GenisysPro.git
+# Move necessary files for building
 cp Doxyfile GenisysPro/Doxyfile
 cp README.md GenisysPro/README.md
 cd GenisysPro
+# Get Doxygen binary
 wget http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.13.linux.bin.tar.gz
 tar -xvf doxygen-1.8.13.linux.bin.tar.gz
 cp doxygen-1.8.13/bin/doxygen doxygen
+# Build documentation
 ./doxygen Doxyfile
 
 if ls html/index.html >/dev/null 2>&1; then
